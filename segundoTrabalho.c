ac50449ae2c30d3feab7467107da8e8b39593d23
bfsgr <bfsgr.code@protonmail.com>
2024-03-24 16:56:38 -0300

    add srand(time)

--- segundoTrabalho.c
+++ segundoTrabalho.c
@@ -631,6 +631,8 @@ void node(int id, int np, int number_of_lines, int number_of_columns) {
 int main(int argc, char *argv[]) {
   int id, np;
 
+  srand(time(NULL));
+
   MPI_Init(&argc, &argv);
   MPI_Comm_rank(MPI_COMM_WORLD, &id);
   MPI_Comm_size(MPI_COMM_WORLD, &np);
